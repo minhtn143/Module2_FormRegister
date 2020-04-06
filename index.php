a6831341259713636d8ce1b19357ef06550b9755
minhtn143 <minhtn143@gmail.com>
2020-04-06 16:15:20 +0700

    upload

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <style>
+        .error {
+            color: #FF0000;
+        }
+        table{
+            border-collapse: collapse;
+            width: 100%;
+        }
+        td, th{
+            border: solid 1px #ccc;
+        }
+        form{
+            width: 450px;
+        }
+    </style>
+</head>
+<body>
+<?php include 'action.php'; ?>
+
+<h2>Registration Form</h2>
+<form method="post">
+    <fieldset>
+        <legend>Details</legend>
+        Name: <input type="text" name="name" value="<?php echo $name; ?>">
+        <span class="error">* <?php echo $nameErr; ?></span>
+        <br><br>
+        E-mail: <input type="text" name="email" value="<?php echo $email; ?>">
+        <span class="error">* <?php echo $emailErr; ?></span>
+        <br><br>
+        Phone: <input type="text" name="phone" value="<?php echo $phone; ?>">
+        <span class="error">*<?php echo $phoneErr; ?></span>
+        <br><br>
+        <input type="submit" name="submit" value="Register">
+        <p><span class="error">* required field.</span></p>
+    </fieldset>
+</form>
+
+<?php
+$registrations = loadRegistrations('data.json');
+?>
+<h2>Registration list</h2>
+<table>
+    <tr>
+        <th>Name</th>
+        <th>Email</th>
+        <th>Phone</th>
+    </tr>
+    <?php foreach ($registrations as $registration): ?>
+        <tr>
+            <td><?= $registration['name']; ?></td>
+            <td><?= $registration['email']; ?></td>
+            <td><?= $registration['phone']; ?></td>
+        </tr>
+    <?php endforeach; ?>
+</table>
+</body>
+</html>
